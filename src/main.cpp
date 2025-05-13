db8d8d7b69b9671c6a947eb9ad15b66e3e2980d1
Hovo751 <159177058+Hovo751@users.noreply.github.com>
2025-05-13 19:42:53 +0400

    added libararyes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include "Headers/Chain.h"
+#include <thread>
+#include <iostream>
+#include <atomic>
+
+unsigned int width, height;
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+    width = sf::VideoMode::getDesktopMode().size.x;
+    height = sf::VideoMode::getDesktopMode().size.y;
+
+    auto window = sf::RenderWindow(sf::VideoMode({ width, height}), "CMake SFML Project", sf::State::Fullscreen);
+    //window.setFramerateLimit(60);
 
     while (window.isOpen())
     {
